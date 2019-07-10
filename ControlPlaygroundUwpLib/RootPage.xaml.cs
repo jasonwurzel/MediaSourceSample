a9c8752631d551e943cc32edb2ad5fc3a2f7183d
Michael Kreich <michaelkreich@hotmail.com>
2019-07-10 09:46:30 +0200

    no navigationview needed...

--- ControlPlaygroundUwpLib/RootPage.xaml.cs
+++ ControlPlaygroundUwpLib/RootPage.xaml.cs
@@ -1,26 +1,10 @@
-﻿using Windows.UI.Xaml.Controls;
-
-namespace ControlPlaygroundUwpLib
+﻿namespace ControlPlaygroundUwpLib
 {
     public sealed partial class RootPage
     {
         public RootPage()
         {
             InitializeComponent();
-
-            frame.Navigate(typeof(PlaygroundPage));
-        }
-
-        private void NavigationView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
-        {
-            string content = args.InvokedItemContainer.Content as string;
-
-            switch (content)
-            {
-                case "playground":
-                    frame.Navigate(typeof(PlaygroundPage));
-                    break;
-            }
         }
     }
 }
